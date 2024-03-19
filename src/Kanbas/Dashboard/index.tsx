ffc1a53d004bfb953a1428271c04ed5abfb8168a
Amaan Ali <amaanali2002@hotmail.com>
2024-03-19 16:57:07 -0400

    3.1

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,10 +1,80 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import courses from "../Database/courses.json";
+import dbCourses from "../Database/courses.json";
 
 function Dashboard() {
+    const [courses, setCourses] = useState(dbCourses);
+    const [course, setCourse] = useState({
+        _id: "0",
+        name: "New Course",
+        number: "New Number",
+        startDate: "2023-09-10",
+        endDate: "2023-12-15",
+        image: "/images/reactjs.jpg",
+    });
+
+    const addNewCourse = () => {
+        const newCourse = { ...course, _id: new Date().getTime().toString() };
+        setCourses([...courses, { ...course, ...newCourse }]);
+    };
+
+    const deleteCourse = (courseId: string) => {
+        setCourses(courses.filter((course) => course._id !== courseId));
+    };
+
+    const updateCourse = () => {
+        setCourses(
+            courses.map((c) => {
+                if (c._id === course._id) {
+                    return course;
+                } else {
+                    return c;
+                }
+            })
+        );
+    };
+
     return (
         <div>
             <h1>Dashboard</h1>
+
+            <h5>Course</h5>
+            <input
+                value={course.name}
+                className="form-control"
+                onChange={(e) => setCourse({ ...course, name: e.target.value })}
+            />
+            <input
+                value={course.number}
+                className="form-control"
+                onChange={(e) =>
+                    setCourse({ ...course, number: e.target.value })
+                }
+            />
+            <input
+                value={course.startDate}
+                className="form-control"
+                type="date"
+                onChange={(e) =>
+                    setCourse({ ...course, startDate: e.target.value })
+                }
+            />
+            <input
+                value={course.endDate}
+                className="form-control"
+                type="date"
+                onChange={(e) =>
+                    setCourse({ ...course, endDate: e.target.value })
+                }
+            />
+            <button className="btn btn-primary" onClick={addNewCourse}>
+                Add
+            </button>
+
+            <button className="btn btn-primary" onClick={updateCourse}>
+                Update
+            </button>
+
             <hr />
             <h2>Published Courses ({courses.length})</h2>
             <hr />
@@ -33,6 +103,25 @@ function Dashboard() {
                                         }}
                                     >
                                         {course.name}
+
+                                        <button
+                                            className="btn btn-primary"
+                                            onClick={(event) => {
+                                                event.preventDefault();
+                                                setCourse(course);
+                                            }}
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            className="btn btn-primary"
+                                            onClick={(event) => {
+                                                event.preventDefault();
+                                                deleteCourse(course._id);
+                                            }}
+                                        >
+                                            Delete
+                                        </button>
                                     </Link>
                                     <p className="card-text">
                                         Full Stack software developer
